a6317f5364e577b950fb4be8e583b30b4c3bc964
Toin <31212492+Toin1779712664@users.noreply.github.com>
2024-08-05 15:49:48 +0800

    Update VarArg.java

--- src/shuo/laoma/basic/c11/VarArg.java
+++ src/shuo/laoma/basic/c11/VarArg.java
@@ -1,11 +1,12 @@
 package shuo.laoma.basic.c11;
 
 public class VarArg {
-
+	// 在C++中该方式被称为可变参方式
 	public static int max(int min, int ... a){
+		// 返回最大的值
 	    int max = min;
 	    for(int i=0;i<a.length;i++){
-	        if(max<a[i]){
+	        if(max<a[i]){	如果当前的值小于a[i]的值那么将max设置为当前最大的这个值
 	            max = a[i];
 	        }
 	    }
@@ -13,10 +14,10 @@ public class VarArg {
 	}
 
 	public static void main(String[] args) {
-	    System.out.println(max(0));
-	    System.out.println(max(0,2));
-	    System.out.println(max(0,2,4));
-	    System.out.println(max(0,2,4,5));
+	    System.out.println(max(0));	// 返回 0
+	    System.out.println(max(0,2)); // 返回 2
+	    System.out.println(max(0,2,4)); // 返回 4
+	    System.out.println(max(0,2,4,5)); // 返回 5
 	}
 
 }
