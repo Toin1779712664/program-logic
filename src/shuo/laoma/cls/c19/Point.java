1ca8ec40df48d70bcb3f56b6703c9bbd6d915104
Toin <31212492+Toin1779712664@users.noreply.github.com>
2024-08-05 17:26:12 +0800

    Update Point.java

--- src/shuo/laoma/cls/c19/Point.java
+++ src/shuo/laoma/cls/c19/Point.java
@@ -12,10 +12,10 @@ public class Point implements MyComparable {
 	public double distance(){
 		return Math.sqrt(x*x+y*y);
 	}
-	
+	// 实现这个接口的功能
 	@Override
 	public int compareTo(Object other) {
-		if(!(other instanceof Point)){
+		if(!(other instanceof Point)){ // 使用instanceof判断传入的参数是否是Point类型,如果不是则抛出IllegalArgumentException异常(如果other是Point类型,那么返回结果为true,通过逻辑非运算符将结果变为false使这if语句不可运行)
 			throw new IllegalArgumentException();
 		}
 		Point otherPoint = (Point)other;
@@ -28,7 +28,7 @@ public class Point implements MyComparable {
 			return 0;
 		}
 	}
-
+	// 重载toStirng方法输出自定义格式
 	@Override
 	public String toString() {
 		return "("+x+","+y+")";
