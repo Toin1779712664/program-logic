72d7151a99e28bb0516e9e89d1e7a8a0d32d3077
Toin <31212492+Toin1779712664@users.noreply.github.com>
2024-08-05 15:37:31 +0800

    Update Basic.java

--- src/shuo/laoma/basic/c11/Basic.java
+++ src/shuo/laoma/basic/c11/Basic.java
@@ -2,11 +2,12 @@ package shuo.laoma.basic.c11;
 
 public class Basic {
 
+	// 求和
 	public static int sum(int a, int b){
 	    int sum = a + b;
 	    return sum;
 	}
-
+	// 打印三个换行
 	public static void print3Lines(){
 	    for(int i=0;i<3;i++){
 	        System.out.println();
@@ -19,10 +20,12 @@ public class Basic {
 	public static void main(String[] args) {
 	    int a = 2;
 	    int b = 3;
+		// 输出：5
 	    int sum = sum(a, b);
 
 	    System.out.println(sum);
 	    print3Lines();
+		// 输出：7
 	    System.out.println(sum(3,4));
 
 	}
