05f211564fd1709b260a8736c4d389f4727b49d3
Toin <31212492+Toin1779712664@users.noreply.github.com>
2024-08-05 15:51:18 +0800

    Update ArrayMax.java

--- src/shuo/laoma/basic/c12/ArrayMax.java
+++ src/shuo/laoma/basic/c12/ArrayMax.java
@@ -4,6 +4,7 @@ public class ArrayMax {
 
     public static int max(int min, int[] arr) {
         int max = min;
+        // 使用foreach方式:返回最大的值
         for(int a : arr){
             if(a>max){
                 max = a;
