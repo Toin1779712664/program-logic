d3d7aee3e98bc0403fc33e92f2ede92a787364cb
Toin <31212492+Toin1779712664@users.noreply.github.com>
2024-08-05 14:38:28 +0800

    Update and rename src/shuo/laoma/basic/c10/Demo.java to src/shuo/laoma/basic/Scanner.java

--- src/shuo/laoma/basic/Scanner.java
+++ src/shuo/laoma/basic/Scanner.java
@@ -8,11 +8,11 @@ public class Demo {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Scanner reader = new Scanner(System.in);
+		Scanner reader = new Scanner(System.in);   // Scanner用于获取输入
 		System.out.println("please input password");
-		int num = reader.nextInt();
+		int num = reader.nextInt();   // 读取整数
 		int password = 6789;
-		while(num!=password){
+		while(num!=password){    // 读取的整数与password进行比较，不然则继续打印密码输入提示直到输入正确密码为止
 		    System.out.println("please input password");
 		    num = reader.nextInt();
 		}
