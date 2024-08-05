4393f29f1054769f12c09f120b81a93ceeecb8b7
Toin <31212492+Toin1779712664@users.noreply.github.com>
2024-08-05 15:47:45 +0800

    Update Recur.java

--- src/shuo/laoma/basic/c11/Recur.java
+++ src/shuo/laoma/basic/c11/Recur.java
@@ -1,17 +1,19 @@
 package shuo.laoma.basic.c11;
 
 public class Recur {
-
+	// 斐波那契递归方式：
 	public static long factorial(int n){
+		// 基线条件，用于退出递归操作
 	    if(n==0){
 	        return 1;
 	    }else{
 	        return n*factorial(n-1);
 	    }
 	}
-	
+	// 斐波那契普通方式
 	public static long factorial2(int n){
 	    long result = 1;
+		// 使用for方式
 	    for(int i=1; i<=n; i++){
 	        result*=i;
 	    }
@@ -22,8 +24,11 @@ public class Recur {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 添加测试
+		long result1=factorial(5);
+		System.out.println(result1);
+		long resulut2=factorial2(5);
+		System.out.println(result2);
 	}
 
 }
