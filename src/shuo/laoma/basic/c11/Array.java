f418268b954d08ddbcd274ffcdff519b0a1d8a46
Toin <31212492+Toin1779712664@users.noreply.github.com>
2024-08-05 15:30:36 +0800

    Update Array.java

--- src/shuo/laoma/basic/c11/Array.java
+++ src/shuo/laoma/basic/c11/Array.java
@@ -10,6 +10,7 @@ public class Array {
 
 	public static void main(String[] args) {
 	    int[] arr = {10,20,30,40};
+		// 修改数组arr的值，输出为0 1 2 3 4
 	    reset(arr);
 	    for(int i=0;i<arr.length;i++){
 	        System.out.println(arr[i]);
