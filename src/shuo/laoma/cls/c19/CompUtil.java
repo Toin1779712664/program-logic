4d959039fbfbf3ea51be714edac9d097ef1df7db
Toin <31212492+Toin1779712664@users.noreply.github.com>
2024-08-05 17:29:29 +0800

    Update CompUtil.java

--- src/shuo/laoma/cls/c19/CompUtil.java
+++ src/shuo/laoma/cls/c19/CompUtil.java
@@ -3,19 +3,21 @@ package shuo.laoma.cls.c19;
 import java.util.Arrays;
 
 public class CompUtil {
+	// 实现数组版本,如果没有为max传入指定的对象则返回null值
 	public static Object max(MyComparable[] objs) {
 		if (objs == null || objs.length == 0) {
 			return null;
 		}
+		// 假设最大值是objs的第一个元素
 		MyComparable max = objs[0];
-		for (int i = 1; i < objs.length; i++) {
+		for (int i = 1; i < objs.length; i++) { // 将遍历数组,且使用MyComparable接口的compareTo方法进行比较
 			if (max.compareTo(objs[i]) < 0) {
 				max = objs[i];
 			}
 		}
 		return max;
 	}
-
+	// 排序算法
 	public static void sort(MyComparable[] objs) {
 		for (int i = 0; i < objs.length; i++) {
 			int min = i;
@@ -24,7 +26,7 @@ public class CompUtil {
 					min = j;
 				}
 			}
-			if (min != i) {
+			if (min != i) {	// 交换两个值
 				MyComparable temp = objs[i];
 				objs[i] = objs[min];
 				objs[min] = temp;
