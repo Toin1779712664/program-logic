873e7f2518b988f8cbc9065cc25f5ee4a453809b
Toin <31212492+Toin1779712664@users.noreply.github.com>
2024-08-07 17:07:54 +0800

    Update Base.java

--- src/shuo/laoma/cls/c20/Base.java
+++ src/shuo/laoma/cls/c20/Base.java
@@ -1,10 +1,10 @@
 package shuo.laoma.cls.c20;
-
+    // 调用AbstractAdder接口,并在当前文件中实现add方法
 public class Base extends AbstractAdder {
     private static final int MAX_NUM = 1000;
     private int[] arr = new int[MAX_NUM];
     private int count;
-    
+    // add方法对传入进来的number进行存放到arr数组的操作
     @Override
     public void add(int number){
         if(count<MAX_NUM){
