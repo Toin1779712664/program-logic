b3aee083d046c2f85511eddafe2362043eb57ba8
Toin <31212492+Toin1779712664@users.noreply.github.com>
2024-08-07 15:33:39 +0800

    Update AbstractAdder.java

--- src/shuo/laoma/cls/c20/AbstractAdder.java
+++ src/shuo/laoma/cls/c20/AbstractAdder.java
@@ -1,5 +1,5 @@
 package shuo.laoma.cls.c20;
-
+    // 定义抽象类,该抽象类实现接口addAll方法,该方法又调用了add方法
 public abstract class AbstractAdder implements IAdd {
     @Override
     public void addAll(int[] numbers) {
