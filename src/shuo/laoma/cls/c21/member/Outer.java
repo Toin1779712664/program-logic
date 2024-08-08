9e30cb64ceb6436669c08455f59696f5631e26e7
Toin <31212492+Toin1779712664@users.noreply.github.com>
2024-08-08 15:24:55 +0800

    Update Outer.java

--- src/shuo/laoma/cls/c21/member/Outer.java
+++ src/shuo/laoma/cls/c21/member/Outer.java
@@ -18,4 +18,5 @@ public class Outer {
         Inner inner = new Inner();
         inner.innerMethod();
     }
+    // 这是一个简单的代码示例，该代码声明一个内部类Inner并定义了innerMethod方法，该方法调用外部定义的action方法。在使用inner.innerMethod()时它将先打印action里的内容记者在输出innerMethod()里的内容
 }
