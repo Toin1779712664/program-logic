9eb79be991bffeaa638766d7f2cbd7eb81e6e204
Toin <31212492+Toin1779712664@users.noreply.github.com>
2024-08-08 15:21:52 +0800

    Update Outer.java

--- src/shuo/laoma/cls/c21/anonymous/Outer.java
+++ src/shuo/laoma/cls/c21/anonymous/Outer.java
@@ -1,11 +1,11 @@
 package shuo.laoma.cls.c21.anonymous;
-
+ // 导入自定义的Point类
 import shuo.laoma.cls.c14.Point;
 
 public class Outer {
     public void test(final int x, final int y){
         Point p = new Point(2,3){                
-                                               
+            // 重写distance方法使其返回new Point的结果                                   
             @Override                              
             public double distance() {             
                 return distance(new Point(x,y));     
