17e539df41d8270f47fbd21aadb1eab2d7109ca9
Toin <31212492+Toin1779712664@users.noreply.github.com>
2024-08-20 09:10:24 +0800

    Update Demo.java

--- src/shuo/laoma/basic/c5/Demo.java
+++ src/shuo/laoma/basic/c5/Demo.java
@@ -8,7 +8,7 @@ public class Demo {
 	public static void main(String[] args) {
 		float f = 0.1f*0.1f;
 		System.out.println(f);
-
+		// 浮点数运算误差
 		
 		System.out.println(0.1f+0.1f);  
 		System.out.println(0.1f*0.1f);
