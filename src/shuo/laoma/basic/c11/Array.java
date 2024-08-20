c1bd12f6ad6b003d822ff50a2c5438b4f31c1115
Toin <31212492+Toin1779712664@users.noreply.github.com>
2024-08-20 09:52:16 +0800

    Update Array.java

--- src/shuo/laoma/basic/c11/Array.java
+++ src/shuo/laoma/basic/c11/Array.java
@@ -1,7 +1,7 @@
 package shuo.laoma.basic.c11;
 
 public class Array {
-
+		// 数组遍历，将对应下标的数组设置为索引
 	public static void reset(int[] arr){
 	    for(int i=0;i<arr.length;i++){
 	        arr[i] = i;
@@ -12,6 +12,7 @@ public class Array {
 	    int[] arr = {10,20,30,40};
 		// 修改数组arr的值，输出为0 1 2 3 4
 	    reset(arr);
+		// 输出修改之后的数组
 	    for(int i=0;i<arr.length;i++){
 	        System.out.println(arr[i]);
 	    }
